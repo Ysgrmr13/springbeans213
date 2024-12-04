4bf838cfa601c1f4d0ffee15ef7d6adedeb683a0
Ysgrmr13 <Ysgrmr13@gmail.com>
2024-12-04 14:26:34 +0300

    change Time

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -14,15 +14,17 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
+    public Timer getTimer() {
+        return timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
-    public Timer getTimer() {
-        return timer;
-    }
+
 }
